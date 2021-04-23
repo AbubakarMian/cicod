6475aea4758772ff2404b759ea7ea44d85ec6cf4
AbubakarMian <33312651+aukhan288@users.noreply.github.com>
2021-04-23 14:46:59 +0500

    tab navigator working

--- app/views/Login.js
+++ app/views/Login.js
@@ -51,12 +51,12 @@ class Login extends React.Component {
 
         // ()=>this.props.navigation.navigate('DrawerNavigation')
         if (this.state.tenantId === '') {
-            alert("tenantId required")
+            alert("Domain required")
         }
         else if (this.state.username === '') {
-            alert("email required")
+            alert("Email required")
         } else if (this.state.password === '') {
-            alert("password required")
+            alert("Password required")
         }
 
         else {
@@ -84,7 +84,7 @@ class Login extends React.Component {
                 lastname: "sandbox last", //responseJson.user.lastname,
                 email: "cicodsandbox@yopmail.com",//responsejson.user.email,
                 phone: "123314324",//responseJson.user.phone,
-                access_token: "Bearer xfE20TOnDN6uhpzMScBj",  //+ responseJson.token
+                access_token: "Bearer 7daIXoCplSUt50mLrx86",  //+ responseJson.token
             });
             this.setState({ Spinner: false })
             this.props.navigation.navigate('Home')
